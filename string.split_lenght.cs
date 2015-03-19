fe3d2c07a055e3a134666468fd6f956978472b3b
Dino <dino.musliu@gmail.com>
2015-03-19 19:16:30 +0100

    Create string.split_lenght.cs

---
+++ string.split_lenght.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            string[] splitNumbers = number.Split('.');
+            int limit0 = Convert.ToInt32(splitNumbers[0]);
+            for(int i=0;i<limit0;i++){
+                Console.WriteLine("x");
+            }
+
+            Console.WriteLine(splitNumbers[1].Length);
+            Console.ReadKey();
+        }
+    }
+}
