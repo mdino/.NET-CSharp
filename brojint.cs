ccae7c0ced1400ad7730715bfbd1e48be02c7316
Dino <dino.musliu@gmail.com>
2015-03-20 13:07:30 +0100

    Create brojint.cs

---
+++ brojint.cs
@@ -0,0 +1,6 @@
+public void unesi
+
+string strbroj=broj.toString();
+int pozicijaTocke= strBroj.IndexOf('.');
+brojDecimala?strBroj.Lenght = (pozicijaTocke+1);
+Broj=(int)((decimal(nroj * (decimalMath.Pow(10,BrojDecimala));
