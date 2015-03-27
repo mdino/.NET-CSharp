c37d96e4fdbb1028c78dee8ae1f40a8933c8cf95
Dino <dino.musliu@gmail.com>
2015-03-27 13:16:04 +0100

    Create nukleoispravi.cs

---
+++ nukleoispravi.cs
@@ -0,0 +1,8 @@
+PI bio je onaj primjer sa nukleus parovima koji je jedna grupa imala prosli tjedan na vjezbama, nisam uspio iskoporati kod ali greske su:
+1. tamo di se deklarira lista treba dodati = new List <Par>();
+2. kod wile petlje za izbornik je ; koju je trebalo maknuti
+3. u if petlji di provjerava koje slovo si unio umjesto && treba bit ||
+4. u for petlji za ispis je stavljeno i = 1, i = i +2, treba biti i=0 i i++
+5. isto u toj for petlji se ispisuje nukleus1, a treba se ispisati nukleus2 ili kak su se vec varijable zvale
+6. u switch di su definirani parovi, su krivo definirani parovi trebabiti ako je A onda je T ak je T onda je A ak je C onda je G i ak je G onda je C
+mislim da su to sve greske, valjda vam bude pomogl
